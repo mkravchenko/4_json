bb0c7caa7ca64fa4431879286753183f4aee155e
mkravchenko <m.kravchenko18@gmail.com>
2016-09-25 23:51:30 +0300

    task 4th is completed

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# 4_json
+This script will print json to pretty view.
+
+How to run:
+1. Download file:
+http://data.mos.ru/opendata/7710881420-magaziny-alkogolnye-napitki
+2. Unzip file and get path to file.
+3. Run program:
+python pprint_json.py <path_to_file.json>
--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,21 @@
 import json
+import os
+import sys
 
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, "r", encoding="utf-8") as f:
+        data = f.read()
+    return json.loads(data)
 
 
-def pretty_print_json(data):
-    pass
+def pretty_print_json(jdata):
+    print(json.dumps(jdata, indent=4, sort_keys=True, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    pass
+    path_to_file = sys.argv[1]
+    json_data = load_data(path_to_file)
+    pretty_print_json(json_data)
